5de4f259e190dfefba1625b0bd48b2e5573bebf8
Martin Svalin <martin@lite.nu>
2011-09-18 14:06:09 +0200

    Refactored spec with custom matcher #be_at(coord)

--- ruby/life_spec.rb
+++ ruby/life_spec.rb
@@ -6,8 +6,7 @@ describe "Any live cell with fewer than two live neighbours dies, as if caused b
     before { subject.add_cell(0,0) }
     it "has a cell at 0,0 before the tick" do
       subject.cells.count.should == 1
-      subject.cells.first.x.should == 0
-      subject.cells.first.y.should == 0
+      subject.cells.first.should be_at(0,0)
     end
   end
 end
@@ -15,3 +14,9 @@ end
 describe "Any live cell with two or three live neighbours lives on to the next generation."
 describe "Any live cell with more than three live neighbours dies, as if by overcrowding."
 describe "Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction."
+
+RSpec::Matchers.define :be_at do |x, y|
+  match do |cell|
+    cell.x == x and cell.y == y
+  end
+end
