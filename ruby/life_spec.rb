6d52b9003c0c864504d06bcb75f4f96ef23b8d93
Martin Svalin <martin@lite.nu>
2011-09-18 13:54:51 +0200

    Added the first failing test
    A game with one cell at 0,0 should have that cell (and only that cell) in #cells

--- ruby/life_spec.rb
+++ ruby/life_spec.rb
@@ -1,6 +1,17 @@
 require './life'
 
-describe "Any live cell with fewer than two live neighbours dies, as if caused by under-population."
+describe "Any live cell with fewer than two live neighbours dies, as if caused by under-population." do
+  describe "A game with only one live cell at coordinates 0,0" do
+    subject { Life.new }
+    before { subject.add_cell(0,0) }
+    it "has a cell at 0,0 before the tick" do
+      subject.cells.count.should == 1
+      subject.cells.first.x.should == 0
+      subject.cells.first.y.should == 0
+    end
+  end
+end
+
 describe "Any live cell with two or three live neighbours lives on to the next generation."
 describe "Any live cell with more than three live neighbours dies, as if by overcrowding."
 describe "Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction."
