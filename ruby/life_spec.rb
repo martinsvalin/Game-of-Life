37f4f0951df666fe2271510be48dc994e404645e
Martin Svalin <martin@lite.nu>
2011-09-18 14:23:59 +0200

    New scenario: two adjacent cells, check #cells content
    Uncovered a bug with this spec. It fails because our cells were created with x=y, y=y.

--- ruby/life_spec.rb
+++ ruby/life_spec.rb
@@ -3,15 +3,26 @@ require './life'
 describe "Any live cell with fewer than two live neighbours dies, as if caused by under-population." do
   describe "A game with only one live cell at coordinates 0,0" do
     subject { Life.new }
-    before { subject.add_cell(0,0) }
+    before { subject.add_cell 0,0 }
     it "has a cell at 0,0 before the tick" do
       subject.cells.count.should == 1
-      subject.cells.first.should be_at(0,0)
+      subject.cells.first.should be_at 0,0
     end
 
     it "has no survivors after one tick" do
       subject.tick!
       subject.cells.should be_empty
+    end
+  end
+
+  describe "A game with two adjacent live cells, at coordinates 0,0 and 1,0" do
+    subject { Life.new }
+    before { subject.add_cell 0,0 }
+    before { subject.add_cell 1,0 }
+    it "has two cells, at 0,0 and 1,0 before the tick" do
+      subject.cells.count.should == 2
+      subject.cells.first.should be_at 0,0
+      subject.cells.last.should be_at 1,0
     end
   end
 end
