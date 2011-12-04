efebb936822f4cddaa8b287cea9ffd6fdcb3a492
Martin Svalin <martin@lite.nu>
2011-12-04 20:25:45 +0100

    Reorganize Generation specs by method

--- ruby/spec/generation_spec.rb
+++ ruby/spec/generation_spec.rb
@@ -1,30 +1,37 @@
 require './lib/generation'
 
 describe "Generation" do
-  let(:gen) { Generation.new }
+  describe "#cells" do
+    context "with no living cells" do
+      let(:gen) { Generation.new }
 
-  it "ticks to the next generation" do
-    gen.tick.should be_instance_of Generation
-  end
+      it "reports its cells" do
+        gen.cells.should be_empty
+      end      
+    end    
 
-  it "reports its cells" do
-    gen.cells.should be_empty
-  end
+    context "with one living cell" do
+      let(:gen) { Generation.new [[0,0]] }
 
-  context "with one living cell at 0,0" do
-    let(:gen) { Generation.new [[0,0]] }
+      it "reports its cell" do
+        gen.cells.should =~ [[0,0]]
+      end
+    end
 
-    it "reports its cell" do
-      gen.cells.should =~ [[0,0]]
+    context "with a still life block of living cells" do
+      let(:cells) { [[0,0], [0,1], [1,0], [1,1]] }
+      let(:gen) { Generation.new cells }
+
+      it "reports its cells" do
+        gen.cells.should =~ cells
+      end
     end
   end
 
-  context "with a still life block of living cells" do
-    let(:cells) { [[0,0], [0,1], [1,0], [1,1]] }
-    let(:gen) { Generation.new cells }
-
-    it "reports its cells" do
-      gen.cells.should =~ cells
+  describe "#tick" do
+    let(:gen) { Generation.new }
+    it "returns the next generation" do
+      gen.tick.should be_instance_of Generation
     end
   end
 end
