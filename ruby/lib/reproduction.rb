6a433363ee06eec6e7205bcf56d9efe8b6a70550
Martin Svalin <martin@lite.nu>
2011-12-04 23:14:39 +0100

    Refactor: short, focused methods

--- ruby/lib/reproduction.rb
+++ ruby/lib/reproduction.rb
@@ -2,9 +2,11 @@ require './lib/rule'
 
 class Reproduction < Rule
   def apply
-    potential = cells.inject([]) do |potential, cell|
-      potential + Neighbourhood.cells_around(*cell)
-    end
     (potential.uniq - cells).select {|cell| neighbour_count(cell) == 3}
+  end
+
+  private
+  def potential
+    cells.inject([]) {|pot, cell| pot + Neighbourhood.cells_around(*cell) }
   end
 end
