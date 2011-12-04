f3595a4cace0cd0e62e589f2a80674cea85ee5bd
Martin Svalin <martin@lite.nu>
2011-12-04 21:08:19 +0100

    Survival with a block of four cells

--- ruby/lib/survival.rb
+++ ruby/lib/survival.rb
@@ -1,9 +1,21 @@
 class Survival
+  attr_reader :cells
+
+  def initialize(cells)
+    @cells = cells
+  end
+
   def self.apply(cells)
     
   end
 
   def apply
-    []
+    cells.select do |cell|
+      neighbour_count(cell) > 1
+    end
+  end
+
+  def neighbour_count(cell)
+    0
   end
 end
--- ruby/spec/survival_spec.rb
+++ ruby/spec/survival_spec.rb
@@ -7,4 +7,13 @@ describe "Survival" do
       rule.apply.should be_empty
     end
   end
+
+  context "with a block of four cells" do
+    let(:cells) { [[0,0], [0,1], [1,0], [1,1]] }
+    let(:rule) { Survival.new cells }
+    it "survives" do
+      rule.should_receive(:neighbour_count).exactly(4).times.and_return(3)
+      rule.apply.should =~ cells
+    end
+  end
 end
